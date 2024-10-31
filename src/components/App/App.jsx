9b15a19f0006437bf0d479d2590715e8e8839bfd
frasdor <frasunkiewicz.dorota@gmail.com>
2024-10-31 14:02:57 +0100

    added contactThunks

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,42 +1,30 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { nanoid } from 'nanoid';
 import ContactForm from '../ContactForm/ContactForm';
 import ContactList from '../ContactList/ContactList';
 import Filter from '../Filter/Filter';
-import { addContact, deleteContact, setFilter } from '../../redux/contacts/contactsSlice';
+import { fetchContacts, addContact, deleteContact } from '../../redux/contacts/contactsThunks';
+import { setFilter } from '../../redux/contacts/contactsSlice';
 import styles from './App.module.css';
 
 const App = () => {
   const dispatch = useDispatch();
   const contacts = useSelector(state => state.contacts.contacts);
   const filter = useSelector(state => state.contacts.filter);
+  const loading = useSelector(state => state.contacts.loading);
 
   useEffect(() => {
-    const savedContacts = localStorage.getItem('contacts');
-    if (savedContacts) {
-      JSON.parse(savedContacts).forEach(contact => dispatch(addContact(contact)));
-    }
+    dispatch(fetchContacts());
   }, [dispatch]);
 
-  useEffect(() => {
-    localStorage.setItem('contacts', JSON.stringify(contacts));
-  }, [contacts]);
-
   const handleAddContact = ({ name, number }) => {
-    const contact = {
-      id: nanoid(),
-      name,
-      number,
-    };
-
     const isExistingContact = contacts.some(c => c.name.toLowerCase() === name.toLowerCase());
     if (isExistingContact) {
       alert(`${name} is already in contacts.`);
       return;
     }
 
-    dispatch(addContact(contact));
+    dispatch(addContact({ name, number }));
   };
 
   const handleDeleteContact = (id) => {
@@ -61,7 +49,11 @@ const App = () => {
 
       <h2>Contacts</h2>
       <Filter value={filter} onChange={handleFilterChange} />
-      <ContactList contacts={filteredContacts} onDeleteContact={handleDeleteContact} />
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        <ContactList contacts={filteredContacts} onDeleteContact={handleDeleteContact} />
+      )}
     </div>
   );
 };
